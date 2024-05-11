c49e3039ae7fbc858ec96425e4ab100029ccf570
Mihiri Priyabhashanie OU <mihirip.ou@mobitel.lk>
2024-05-11 07:04:42 +0530

    createpartner_fron_complete

--- app/Components/PartnerForm.tsx
+++ app/Components/PartnerForm.tsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 import InputField from '../Components/InputField';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import { Button, ButtonGroup } from "@nextui-org/button";
+import router from 'next/router';
+import { useRouter } from 'next/navigation'
 
 const PartnerForm: React.FC = () => {
   const [partnerId, setPartnerId] = useState('');
   const [partnerName, setPartnerName] = useState('');
+  const [partnerUsername, setPartnerUsername] = useState('');
   const [partnerEmail, setPartnerEmail] = useState('');
   const [partnerMobileNumber, setPartnerMobileNumber] = useState('');
   const [partnerCountry, setPartnerCountry] = useState('');
@@ -43,12 +46,15 @@ const PartnerForm: React.FC = () => {
     }
 
   };
+   
 
   const isValidEmail = (email: string) => {
     // Regular expression for validating email addresses
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
+
+  const router = useRouter()
   return (
     <div className="w-full  px-1 grid grid-cols-1 gap-4 ">
       <div>
@@ -64,43 +70,54 @@ const PartnerForm: React.FC = () => {
             value={partnerId}
             onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerId(e.target.value)}
             error={''}
+            
           />
           <InputField
             label="Partner Name"
             value={partnerName}
             onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerName(e.target.value)}
-            error={partnerNameError} // Pass the error state to indicate errors
+            error={partnerNameError} 
+            infoMessage='Must be at least 6 characters long and less than 20 characters'
           />
-       
+          <InputField
+            label="Partner Username"
+            value={partnerUsername}
+            onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerUsername(e.target.value)}
+            error={partnerNameError} 
+            infoMessage='Must be at least 6 characters long and less than 20 characters'
+          />
           <InputField
             label="Partner Email"
             value={partnerEmail}
             onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerEmail(e.target.value)}
             error={partnerEmailError}
+            infoMessage='Must be at least 6 characters long and less than 20 characters'
           />
-          <hr className="border-gray-300 w-full mt-6 mb-6" /> {/* Horizontal line */}
+          <hr className="border-gray-300 w-full mt-6 mb-6" /> 
           <div className="mb-3 text-xl underline-color-black underline-offset-1">Contact Information</div>
           <InputField
             label="Mobile number"
             value={partnerMobileNumber}
             onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerMobileNumber(e.target.value)}
             error={partnerMobileNumberError}
+            infoMessage='Must be a number contain 10 characters.'
           />
           <InputField
             label="Country"
             value={partnerCountry}
             onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPartnerCountry(e.target.value)}
             error={partnerCountryError}
+            infoMessage='required'
           />
-          <div className="flex justify-end w-9/12 py-10 ">
+        </form>
+        <div className="flex justify-end w-9/12 py-10 ">
             <div className='place-content-end '>
               <button onClick={handleSubmit}  className="bg-slate-400 hover:bg-transparent text-black-800 font-semibold hover:text-bslack py-2 px-4 border border-stone-400 hover:border-slate-700 rounded">Submit</button>
             </div>
             <div className='pl-5'>
-              <button className="bg-stone-400 hover:bg-transparent text-black-800 font-semibold hover:text-black py-2 px-4 border border-slate-400 hover:border-slate-700 rounded">Cancel</button>
+              <button onClick={() => router.push('/')} className="bg-stone-400 hover:bg-transparent text-black-800 font-semibold hover:text-black py-2 px-4 border border-slate-400 hover:border-slate-700 rounded">Cancel</button>
             </div>
           </div>
-        </form>
       </div>
     </div>
   );
